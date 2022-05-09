8e54f8d6467dc16ea2e0bcfcdf69ff788a26d910
kennedn <kennedn@msn.com>
2022-05-09 14:56:42 +0100

    Command and minimalist webhook json

--- openshift/pipeline/commands.sh
+++ openshift/pipeline/commands.sh
@@ -9,4 +9,14 @@ oc create -f pipeline-run.yml
 tkn pipelinerun logs -f
 oc create -f trigger-template.yml -f trigger-binding.yml -f trigger.yml -f event-listener.yml
 EVENT_LISTENER_URL=$(oc get route el-petclinic-build --template='{{printf "http://%s" .spec.host}}')
-curl -X GET -H 'Content-Type: application/JSON' http://el-petclinic-build-liberty-pipeline.apps-crc.testing -d '{}'
+# echo $EVENT_LISTENER_URL
+# http://el-petclinic-build-liberty-pipeline.apps-crc.testing
+curl -sX GET -H 'Content-Type: application/json' "${EVENT_LISTENER_URL}" -d '{}' | jq -r
+{
+  "eventListener": "petclinic-build",
+  "namespace": "liberty-pipeline",
+  "eventListenerUID": "1bbb812b-2525-4fb8-a746-85561e577bf7",
+  "eventID": "3af3315f-4de7-4de5-a778-93512f1b6dac"
+}
+oc logs deploy/el-petclinic-build -f | jq
+curl -sX POST -H 'Content-Type: application/JSON' "${EVENT_LISTENER_URL}" -d@github_webhook.json | jq -r-
